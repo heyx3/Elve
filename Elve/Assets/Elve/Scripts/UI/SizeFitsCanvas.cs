4ad954d1539e0090c0af217375eb123f18a39cee
heyx3 <manning.w@husky.neu.edu>
2015-08-15 01:21:16 -0400

    Fix more UI issues

---
+++ Elve/Assets/Elve/Scripts/UI/SizeFitsCanvas.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+/// <summary>
+/// Makes this UI object's RectTransform match the bounds of a given canvas.
+/// </summary>
+[ExecuteInEditMode]
+public class SizeFitsCanvas : MonoBehaviour
+{
+	public Canvas ToFit;
+
+	private RectTransform toFitTR;
+	private RectTransform tr;
+
+
+	void Awake()
+	{
+		tr = GetComponent<RectTransform>();
+		toFitTR = ToFit.GetComponent<RectTransform>();
+	}
+	void Update()
+	{
+		if (toFitTR == null)
+			tr.position = ToFit.transform.position;
+		else tr.position = toFitTR.position;
+
+		tr.sizeDelta = new Vector2(ToFit.pixelRect.width, ToFit.pixelRect.height);
+	}
+}
